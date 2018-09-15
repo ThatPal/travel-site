906c301c36f93b5f99c4f6e55ba74174d15e85ee
Mordechai Pal <mordechaipal@gmail.com>
2018-09-15 16:54:59 -0400

    Completed sticky header and scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu = new MobileMenu();
 const revealOnScrollFeatures = new RevealOnScroll($('.feature-item'), '85%');
 const revealOnScrollTestimonials = new RevealOnScroll($('.testimonial'), '60%');
+const stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,65 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
+
+class StickyHeader {
+    constructor() {
+        this.siteHeader = $('.site-header');
+        this.headerTriggerElement = $('.large-hero__title');
+        this.createHeaderWaypoint();
+        this.pageSections = $('.page-section');
+        this.headerLinks = $('.primary-nav a');
+        this.createPageSectionWaypoint();
+        this.addSmoothScrolling();
+    }
+
+    addSmoothScrolling() {
+        this.headerLinks.smoothScroll();
+    }
+
+    createHeaderWaypoint() {
+        var classThis = this;
+        new Waypoint({
+            element: this.headerTriggerElement[0],
+            handler: function(direction) {
+                if(direction == 'down') {
+                    classThis.siteHeader.addClass('site-header--dark');
+                } else {
+                    classThis.siteHeader.removeClass('site-header--dark');
+                }
+            }
+        });
+    }
+
+    createPageSectionWaypoint() {
+        var classThis = this;
+        this.pageSections.each(function() {
+            var currentItem = this;
+            new Waypoint({
+                element: currentItem,
+                handler: function(direction) {
+                    if(direction == 'down') {
+                        var matchingHeaderLink = currentItem.getAttribute('data-matching-link');
+                        classThis.headerLinks.removeClass('is-current-link');
+                        $(matchingHeaderLink).addClass('is-current-link');
+                    }
+                },
+                offset: '18%'
+            });
+
+            new Waypoint({
+                element: currentItem,
+                handler: function(direction) {
+                    if(direction == 'up') {
+                        var matchingHeaderLink = currentItem.getAttribute('data-matching-link');
+                        classThis.headerLinks.removeClass('is-current-link');
+                        $(matchingHeaderLink).addClass('is-current-link');
+                    }
+                },
+                offset: '-40%'
+            });
+        });
+    }
+}
+
+export default StickyHeader;

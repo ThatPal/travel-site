ff54b9c7bba0265ce8d1c6204ab0aad62bd281e2
Mordechai Pal <mordechaipal@gmail.com>
2018-09-16 13:40:39 -0400

    Completed modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 const mobileMenu = new MobileMenu();
 const revealOnScrollFeatures = new RevealOnScroll($('.feature-item'), '85%');
 const revealOnScrollTestimonials = new RevealOnScroll($('.testimonial'), '60%');
 const stickyHeader = new StickyHeader();
+const modal = new Modal();
